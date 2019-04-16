67a4b057328a3e63f6722f89eddc3f04e86f5b08
阿飞 <3045282682@qq.com>
2019-04-16 22:57:35 +0800

    实战

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose
 
 //创建app
 let app = express();
-app.listen(4561);
+app.listen(7894);
 
 
 //设置session参数
@@ -26,7 +26,7 @@ app.use(session({
     ,rolling:true //只要用户和后端有交互（访问页面，跳转页面，ajax……），刷新存储时间
     ,resave:false //是否每次请求都重新存储session数据
     ,saveUninitialized:false //初始值
-    ,cookie : {maxAge:1000*30} //设置session过期时间
+    ,cookie : {maxAge:1000*60*10} //设置session过期时间
     ,store : new mongooseSession({
         url : "mongodb://localhost:27017/blog"
     })//不设置store是服务器内存中存储session信息，我们可以通过设置store讲session数据存到数据库
@@ -42,22 +42,27 @@ app.use(express.static("./public"));
 //设置模板引擎
 app.set("view engine","ejs");
 
-//根路由
+//根 路由
 app.get("/",(req,res)=>{res.render("index")});
 
-//注册路由
-app.get("/reg",(req,res)=>{res.render("reg")});
+//注册 路由
 app.post("/reg",require("./routers/reg"));
 
-//实时检测用户名是否已存在
-//app.post("/repeat",require("./routers/repeat"));
-
-//登录路由
-app.get("/login",(req,res)=>{res.render("login")});
+//登录 路由
 app.post("/login",require("./routers/login"));
+//登出 路由
+app.get("/logout",(req,res)=>{
+    //清除session
+    req.session.destroy();
+    //重定向到首页
+    res.redirect("/");
+});
 
-//用户中心的路由
+//用户中心的路由 //用于检测用户登录与否的路由
 app.get("/usercenter",require("./routers/usercenter"));
+app.post("/usercenter",(req,res)=>{res.send({login:!!req.session.userInfo})});
 
+//更新 路由
+app.post("/update",require("./routers/update"));
 
 
--- routers/usercenter.js
+++ routers/usercenter.js
@@ -5,34 +5,13 @@ const user = require("../model/user");
 module.exports = function(req,res){
 
     //console.log(req.session.goudan);
-    console.log(req.session.userInfo);
+    //console.log(req.session.userInfo);
 
-
-    let data = {};
     if ( req.session.userInfo ){
-        data = req.session.userInfo;
-    } else{
-        data = {_id:null}
-    }
-
-    res.render("usercenter",data);
-
-};
-
-
-
-
-
-/*user.findById(req.cookies._id)
-        .then(data=>{
-            console.log(data);
-
-            if ( data ){
-                res.render("usercenter",{username:data.username});
-            } else{
-                res.render("usercenter",{username:null});
-            }
-
-
+        user.findById(req.session.userInfo._id).then(data=>{
+            res.render("usercenter",data);
         });
-*/
+    } else{
+        res.render("usercenter",{_id:null});
+    }
+};
